b1acd0e2922c835f99be560e42b7cb9a1b65c5ef
palmwi2010 <wop3000@hotmail.co.uk>
2024-08-07 10:21:23 +0100

    Initialized webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist'
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader']
+        }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })]
+};
