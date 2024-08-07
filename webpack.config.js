4e4f108b68d40430f3214645b8cd246365bf6d96
palmwi2010 <wop3000@hotmail.co.uk>
2024-08-07 22:38:36 +0100

    Added webpack change to latest commit

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,11 @@ module.exports = {
               }
             }
           ]
-        }
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
     ]
   },
   plugins: [new HtmlWebpackPlugin({
